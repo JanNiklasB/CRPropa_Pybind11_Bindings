214cb80421c4ac6444e1cfa5313c7867a994b290
Jan-Niklas Bohnensack <jan-niklas.bohnensack@rub.de>
2026-08-27 17:16:27 +0200

    added std and global namespace (needed adjustements on binder source code (see documentation and my fork))

--- python/createBindings.sh
+++ python/createBindings.sh
@@ -5,20 +5,20 @@ headers_to_bind_file=@headers_to_bind_file@
 root_module_name=@root_module_name@
 compile_commands_file=@compile_commands_file@
 
-rm -rf $working_dir/bindings && mkdir $working_dir/bindings
+# rm -rf $working_dir/bindings && mkdir $working_dir/bindings
 grep -rh "#include" $headers_to_bind_file \
 	| sed -E "s/#include\s+\"/#include </g" \
 	| sed -E s/\"/\>/g \
 	| sed -E "s/\s+$//g" \
-	| sort -u \
 	> $working_dir/includes_for_bindings.h
 binder \
 	--root-module $root_module_name \
 	--prefix $working_dir/bindings/ \
 	--bind $root_module_name \
-	--config $working_dir/binder.cfg \
-	--include-pybind11-stl \
+	--bind-class-template-specialization \
 	--annotate-includes \
 	--annotate-functions \
+	--config $working_dir/binder.cfg \
+	--include-pybind11-stl \
 	$working_dir/includes_for_bindings.h \
 	-p $compile_commands_file
--- python/crpropa/__init__.py
+++ python/crpropa/__init__.py
@@ -1,8 +1,3 @@
+from .crpropa import *
+del crpropa
 from .crpropa.crpropa import *
-from .crpropa import declare_version
-from .crpropa import logError
-from .crpropa import logInfo
-from .crpropa import logWarning
-from .crpropa import logDebug
-from .crpropa import setLogStream
-from .crpropa import setLogLevel
